58209f022ec07d95369187b59b36c638754ddc46
Alex <ailatzis@gmail.com>
2023-02-24 22:18:27 +0200

    test
    test

--- Main.java
+++ Main.java
@@ -1,3 +1,8 @@
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
 import java.util.*;
 
 
@@ -5,5 +10,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("another hello!!");
+
+
+        ///TEST API
+        String result = Api.findMeal("Saganaki");
+        System.out.println(result);
+        result= Api.findMealsCategories();
+        System.out.println(result);
+        result = Api.findMealsByCategory("Seaffod");
     }
+
+
 }

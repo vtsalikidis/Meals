1489560239a89be5d39142ec4938443f9af4ebf3
vtsalikidis <126190473+vtsalikidis@users.noreply.github.com>
2023-02-27 02:10:33 +0200

    test
    test

--- Main.java
+++ Main.java
@@ -15,6 +15,7 @@ public class Main {
         System.out.println("hello");
         System.out.println("another hello!!");
         //createTableAndData();
+        new MainScreen();
     }
 
 
---
+++ ShowMealsScreen.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class ShowMealsScreen extends JFrame {
+    private JList list1;
+    private JTextArea textArea1;
+    private JTextArea textArea2;
+    private JTextArea textArea3;
+    private JTextArea textArea4;
+    private JLabel titleShowMealsfrm;
+    private JButton αποθήκευσηButton;
+    private JButton επεξεργασίαButton;
+}

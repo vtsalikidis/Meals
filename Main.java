389be6df2a05fd91a347dadb970394b98fd58339
vtsalikidis <126190473+vtsalikidis@users.noreply.github.com>
2023-03-04 01:20:44 +0200

    r
    r

--- Main.java
+++ Main.java
@@ -14,8 +14,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("another hello!!");
-
-       // createTableAndData();
+        createTableAndData();
         MainScreen.getInstance();
     }
 
